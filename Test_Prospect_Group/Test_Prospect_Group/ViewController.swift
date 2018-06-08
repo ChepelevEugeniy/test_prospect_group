eb1c7ab773817fdc15f800211fbf59ed3394875f
Maxim Biriukov <maxim.biriukov@vilmate.com>
2018-06-08 19:43:12 +0300

    Initial commit. Create start project

---
+++ Test_Prospect_Group/Test_Prospect_Group/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test_Prospect_Group
+//
+//  Created by Maxim Biriukov on 6/8/18.
+//  Copyright © 2018 Eugene Chepelev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
